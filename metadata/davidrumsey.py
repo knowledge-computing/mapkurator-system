83cfdf46cfff36c43a65a31c3746738f49b9e1be
Jina Kim <jinakimcool@gmail.com>
2022-04-20 19:16:21 -0500

    Update metadata modules

---
+++ metadata/davidrumsey.py
@@ -0,0 +1,25 @@
+import requests
+
+class DavidRumsey():
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.headers = {
+            'Authorization': self.api_key,
+            'Content-Type': 'application/json',
+            'charset': 'utf-8'
+            }
+
+    def get_ground_control_points(self, external_id):
+        """
+        Get ground control points via Oldmapsonline API.
+        Args:
+            external_id: str
+        Returns:
+            gcps: list
+
+            All pairs of ground control points
+            e.g., [{'location': , 'pixel': }, ]
+        """
+
+
+        return
