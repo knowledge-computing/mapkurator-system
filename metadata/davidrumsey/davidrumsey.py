b4930ae2f2e548755604082c305abedbba499ea5
Jina Kim <jinakimcool@gmail.com>
2022-04-20 21:05:33 -0500

    Update metadata/davidrumsey.py

--- metadata/davidrumsey/davidrumsey.py
+++ metadata/davidrumsey/davidrumsey.py
@@ -1,8 +1,13 @@
 import requests
+import pandas as pd
+
 
 class DavidRumsey():
-    def __init__(self, api_key):
+    def __init__(self, api_key, csv_filename):
         self.api_key = api_key
+        self.csv_filename = csv_filename
+
+        self.df = pd.read_csv(self.csv_filename)
         self.headers = {
             'Authorization': self.api_key,
             'Content-Type': 'application/json',
@@ -15,21 +20,33 @@ class DavidRumsey():
         Args:
             external_id: str
         Returns:
+            transform_method: str
+                Transformation method
+                e.g., "affine", "polynomial", "tps"
             gcps: list
-
-            All pairs of ground control points
-            e.g., [{'location': [-118.269356489, 34.063140276], 'pixel': [5629, 5064]},{'location': , 'pixel': }, ... ]
+                All pairs of ground control points
+                e.g., [{'location': [-118.269356489, 34.063140276], 'pixel': [5629, 5064]},{'location': , 'pixel': }, ... ]
         """
 
         # 404 ERROR on many maps
         # 1. GET /maps/external/{external_id}
         # baseurl = "https://api.oldmapsonline.org/1.0/maps/external/" + external_id
         # res = requests.get(baseurl, headers=self.headers)
+        map_id = self.df[self.df['external_id']==external_id]['id']
 
         # 2. GET /maps/{id}/georeferences
-        # baseurl = "https://api.oldmapsonline.org/1.0/maps/" + map_id + "/georeferences"
-        # res = requests.get(baseurl, headers=self.headers)
+        baseurl = "https://api.oldmapsonline.org/1.0/maps/" + map_id + "/georeferences"
+        res = requests.get(baseurl, headers=self.headers)
 
-        
+        try:
+            res.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            return None
 
-        return
+        data = res.json()
+        if not data['items']:
+            return None
+        else:
+            transform_method = data['items'][0]['transformation_method']
+            gcps = data['items'][0]['gcps']
+            return transform_method, gcps
