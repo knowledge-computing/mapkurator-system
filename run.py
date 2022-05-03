087e871c2a6522178bea50b12beb33a6c0457232
Jina Kim <jinakimcool@gmail.com>
2022-05-02 22:00:26 -0500

    Update m4_geocoordinate_converter

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 import glob
 
-map_kurator_system_dir = '/home/zekun/dr_maps/mapkurator-system/'
-text_spotting_model_dir = '/home/zekun/antique_names/model/AdelaiDet/'
+# map_kurator_system_dir = '/home/zekun/dr_maps/mapkurator-system/'
+map_kurator_system_dir = '/Users/jinak/mapkurator-system/'
+# text_spotting_model_dir = '/home/zekun/antique_names/model/AdelaiDet/'
 sample_map_path = 'm1_geotiff/data/sample_US_jp2_100_maps.csv'
 
 # # run module1 to generate geotiff
@@ -50,15 +51,26 @@ sample_map_path = 'm1_geotiff/data/sample_US_jp2_100_maps.csv'
 
 
 # run module2: geojson stitching
-os.chdir(os.path.join(map_kurator_system_dir ,'m2_detection_recognition'))
-
-
-stitch_input_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/100_maps_crop_outabc/')
+# os.chdir(os.path.join(map_kurator_system_dir ,'m2_detection_recognition'))
+#
+#
+# stitch_input_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/100_maps_crop_outabc/')
 stitch_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/100_maps_geojson_abc/')
-if not os.path.isdir(stitch_output_dir):
-    os.makedirs(stitch_output_dir)
-run_stitch_command = 'python stitch_output.py --input_dir '+stitch_input_dir + ' --output_dir ' + stitch_output_dir 
-print(run_stitch_command)
-os.system(run_stitch_command)
+# if not os.path.isdir(stitch_output_dir):
+#     os.makedirs(stitch_output_dir)
+# run_stitch_command = 'python stitch_output.py --input_dir '+stitch_input_dir + ' --output_dir ' + stitch_output_dir
+# print(run_stitch_command)
+# os.system(run_stitch_command)
 
-    
+
+
+# run module4: convert image coordinates to geocoordinates
+os.chdir(os.path.join(map_kurator_system_dir, 'm4_geocoordinate_converter'))
+input_csv = os.path.join(map_kurator_system_dir, sample_map_path)
+geojson_output_dir = os.path.join(map_kurator_system_dir, 'm4_geocoordinate_converter', 'data/100_maps_geojson_abc_geocoord/')
+if not os.path.isdir(geojson_output_dir):
+    os.makedirs(geojson_output_dir)
+
+run_converter_command = 'python convert_geojson_to_geocoord.py --sample_map_path '+ input_csv +' --in_geojson_dir '+stitch_output_dir +' --out_geojson_dir '+geojson_output_dir
+print(run_converter_command)
+os.system(run_converter_command)
