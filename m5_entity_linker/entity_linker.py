216175cadf735961a71e8530edf1805cde551192
Jina Kim <jinakimcool@gmail.com>
2022-05-25 16:09:07 -0500

    Update entity_linker.py

---
+++ m5_entity_linker/entity_linker.py
@@ -0,0 +1,78 @@
+import os
+import argparse
+import ast
+from dotenv import load_dotenv
+
+import pandas as pd
+import numpy as np
+
+import geojson
+
+from sqlalchemy import create_engine
+# import reverse_geocode
+import geocoder
+from shapely.geometry import Polygon
+
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+connection_string = f'postgresql://postgres:{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}'
+
+
+def main(args):
+
+    # check if first pair of gcps is in midwest-US
+    conn = create_engine(connection_string, echo=False)
+    sample_map_df = pd.read_csv(args.sample_map_path, dtype={'external_id': str})
+    sample_map_df['external_id'] = sample_map_df['external_id'].str.strip("'").str.replace('.', '')
+    midwest = ["Illinois", "Missouri", "Kansas", "Iowa", "South Dakota", "Indiana", "Ohio", "Wisconsin", "Minnesota", "Michigan"]
+
+    geojson_files = os.listdir(args.in_geojson_dir)[9:10]
+    # print(geojson_files)
+    for i, geojson_file in enumerate(geojson_files):
+        row = sample_map_df[sample_map_df['external_id']==geojson_file.split(".")[0]]
+        gcps = ast.literal_eval(row.iloc[0]['gcps'])
+        geocode = geocoder.osm(gcps[0]['location'][::-1], method='reverse')
+
+        if geocode.state in midwest:
+            with open(args.in_geojson_dir+geojson_file) as f:
+                data = geojson.load(f)
+                for feature_data in data['features']:
+                    pts = np.array(feature_data['geometry']['coordinates']).reshape(-1, 2)
+                    map_polygon = Polygon(pts)
+                    map_text = str(feature_data['properties']['text']).lower()
+                    map_text = map_text.replace("'", "")
+
+                    query = f"""SELECT p.ogc_fid
+                        FROM  polygon_features p
+                        WHERE LOWER(p.name) LIKE '%%{map_text}%%'
+                        AND ST_INTERSECTS(ST_TRANSFORM(ST_SetSRID('{map_polygon}'::geometry, 4326)::geometry, 4326), p.wkb_geometry);
+                    """
+
+                    intersect_df = pd.read_sql(query, con=conn)
+                    if not intersect_df.empty:
+                        feature_data['properties']['osm_ogc_fid'] = intersect_df['ogc_fid'].values.tolist()
+                    # else:
+                    #     feature_data['properties']['osm_ogc_fid'] = []
+
+            with open(args.out_geojson_dir+geojson_file, 'w') as output_geojson:
+                geojson.dump(data, output_geojson)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sample_map_path', type=str, default='data/initial_US_100_maps.csv',
+                        help='path to sample map csv, which contains gcps info')
+    parser.add_argument('--in_geojson_dir', type=str, default='data/100_maps_geojson_abc_geocoord/',
+                        help='input dir for results of M2')
+    parser.add_argument('--out_geojson_dir', type=str, default='data/100_maps_geojson_abc_linked/',
+                        help='output dir for converted geojson files')
+
+    args = parser.parse_args()
+
+    main(args)
--- run.py
+++ run.py
@@ -8,7 +8,7 @@ sample_map_path = 'm1_geotiff/data/sample_US_jp2_100_maps.csv'
 
 # # run module1 to generate geotiff
 # os.chdir(os.path.join(map_kurator_system_dir ,'m1_geotiff'))
-# input_csv = os.path.join(map_kurator_system_dir ,sample_map_path)
+input_csv = os.path.join(map_kurator_system_dir ,sample_map_path)
 # geotiff_output_dir = os.path.join(map_kurator_system_dir ,'m1_geotiff/data/geotiff')
 # if not os.path.isdir(geotiff_output_dir):
 #     os.makedirs(geotiff_output_dir)
@@ -20,32 +20,32 @@ sample_map_path = 'm1_geotiff/data/sample_US_jp2_100_maps.csv'
 
 # run module2: image cropping
 
-geotiff_path_list = glob.glob(os.path.join(geotiff_output_dir, '*.geotiff'))
-assert(len(geotiff_path_list) != 0)
-
-for geotiff_path in geotiff_path_list:
-    os.chdir(os.path.join(map_kurator_system_dir ,'m2_detection_recognition'))
-    map_name = os.path.basename(geotiff_path).split('.')[0]
-
-    cropping_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/100_maps_crop/')
-    if not os.path.isdir(cropping_output_dir):
-        os.makedirs(cropping_output_dir)
-    run_crop_command = 'python crop_img.py --img_path '+geotiff_path + ' --output_dir '+ cropping_output_dir
-    print(run_crop_command)
-    os.system(run_crop_command)
-
-    # run module2: text spotting
-    os.chdir(text_spotting_model_dir)
-    map_name = os.path.basename(geotiff_path).split('.')[0]
-
-    spotting_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/100_maps_crop_outabc/',map_name)
-    if not os.path.isdir(spotting_output_dir):
-        os.makedirs(spotting_output_dir)
-
-    run_spotting_command = 'python demo/demo.py 	--config-file configs/BAText/CTW1500/attn_R_50.yaml 	--input '+ map_kurator_system_dir+'/m2_detection_recognition/data/100_maps_crop/'+map_name+'  --output '+ spotting_output_dir + '   --opts MODEL.WEIGHTS ctw1500_attn_R_50.pth'
-    run_spotting_command  += ' 1> /dev/null'
-    print(run_spotting_command)               
-    os.system(run_spotting_command)
+# geotiff_path_list = glob.glob(os.path.join(geotiff_output_dir, '*.geotiff'))
+# assert(len(geotiff_path_list) != 0)
+#
+# for geotiff_path in geotiff_path_list:
+#     os.chdir(os.path.join(map_kurator_system_dir ,'m2_detection_recognition'))
+#     map_name = os.path.basename(geotiff_path).split('.')[0]
+#
+#     cropping_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/100_maps_crop/')
+#     if not os.path.isdir(cropping_output_dir):
+#         os.makedirs(cropping_output_dir)
+#     run_crop_command = 'python crop_img.py --img_path '+geotiff_path + ' --output_dir '+ cropping_output_dir
+#     print(run_crop_command)
+#     os.system(run_crop_command)
+#
+#     # run module2: text spotting
+#     os.chdir(text_spotting_model_dir)
+#     map_name = os.path.basename(geotiff_path).split('.')[0]
+#
+#     spotting_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/100_maps_crop_outabc/',map_name)
+#     if not os.path.isdir(spotting_output_dir):
+#         os.makedirs(spotting_output_dir)
+#
+#     run_spotting_command = 'python demo/demo.py 	--config-file configs/BAText/CTW1500/attn_R_50.yaml 	--input '+ map_kurator_system_dir+'/m2_detection_recognition/data/100_maps_crop/'+map_name+'  --output '+ spotting_output_dir + '   --opts MODEL.WEIGHTS ctw1500_attn_R_50.pth'
+#     run_spotting_command  += ' 1> /dev/null'
+#     print(run_spotting_command)
+#     os.system(run_spotting_command)
 
     #break
 
@@ -55,7 +55,7 @@ for geotiff_path in geotiff_path_list:
 #
 #
 # stitch_input_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/100_maps_crop_outabc/')
-stitch_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/100_maps_geojson_abc/')
+# stitch_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/100_maps_geojson_abc/')
 # if not os.path.isdir(stitch_output_dir):
 #     os.makedirs(stitch_output_dir)
 # run_stitch_command = 'python stitch_output.py --input_dir '+stitch_input_dir + ' --output_dir ' + stitch_output_dir
@@ -65,12 +65,22 @@ stitch_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recogniti
 
 
 # run module4: convert image coordinates to geocoordinates
-os.chdir(os.path.join(map_kurator_system_dir, 'm4_geocoordinate_converter'))
-input_csv = os.path.join(map_kurator_system_dir, sample_map_path)
+# os.chdir(os.path.join(map_kurator_system_dir, 'm4_geocoordinate_converter'))
 geojson_output_dir = os.path.join(map_kurator_system_dir, 'm4_geocoordinate_converter', 'data/100_maps_geojson_abc_geocoord/')
+# if not os.path.isdir(geojson_output_dir):
+#     os.makedirs(geojson_output_dir)
+#
+# run_converter_command = 'python convert_geojson_to_geocoord.py --sample_map_path '+ input_csv +' --in_geojson_dir '+stitch_output_dir +' --out_geojson_dir '+geojson_output_dir
+# print(run_converter_command)
+# os.system(run_converter_command)
+
+
+# run module5: link entities in OSM
+os.chdir(os.path.join(map_kurator_system_dir, 'm5_entity_linker'))
+geojson_linked_output_dir = os.path.join(map_kurator_system_dir, 'm5_entity_linker', 'data/100_maps_geojson_abc_linked/')
 if not os.path.isdir(geojson_output_dir):
     os.makedirs(geojson_output_dir)
 
-run_converter_command = 'python convert_geojson_to_geocoord.py --sample_map_path '+ input_csv +' --in_geojson_dir '+stitch_output_dir +' --out_geojson_dir '+geojson_output_dir
-print(run_converter_command)
-os.system(run_converter_command)
+run_linker_command = 'python entity_linker.py --sample_map_path '+ input_csv +' --in_geojson_dir '+ geojson_output_dir +' --out_geojson_dir '+ geojson_linked_output_dir
+print(run_linker_command)
+os.system(run_linker_command)
