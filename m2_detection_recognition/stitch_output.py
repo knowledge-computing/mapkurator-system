2a61d7e824eb2a750bbf95cc5ecbf47567de9942
Zekun <li002666@umn.edu>
2022-06-12 18:43:56 -0500

    restructure run.py

--- m2_detection_recognition/stitch_output.py
+++ m2_detection_recognition/stitch_output.py
@@ -11,61 +11,60 @@ logging.basicConfig(level=logging.INFO)
 pd.options.mode.chained_assignment = None
 
 def concatenate_and_convert_to_geojson(args):
-    input_dir = args.input_dir
-    output_dir = args.output_dir
+    map_subdir = args.input_dir
+    output_geojson = args.output_geojson
     shift_size = args.shift_size
 
-    map_subdir_list = os.listdir(input_dir)
-
-    for map_subdir in map_subdir_list:
-        file_list = glob.glob(os.path.join(input_dir, map_subdir) + '/*.json')
-        file_list = sorted(file_list)
-        if len(file_list) == 0:
-            print('No files found for %s' % map_subdir)
+    file_list = glob.glob(map_subdir + '/*.json')
+    file_list = sorted(file_list)
+    if len(file_list) == 0:
+        logging.warning('No files found for %s' % map_subdir)
+        continue
+    map_data = []
+    for file_path in file_list:
+        patch_index_h, patch_index_w = os.path.basename(file_path).split('.')[0].split('_')
+        patch_index_h = int(patch_index_h[1:])
+        patch_index_w = int(patch_index_w[1:])
+        try:
+            df = pd.read_json(file_path)
+        except pd.errors.EmptyDataError:
+            logging.warning('%s is empty. Skipping.' % file_path)
             continue
-        map_data = []
-        for file_path in file_list:
-            patch_index_h, patch_index_w = os.path.basename(file_path).split('.')[0].split('_')
-            patch_index_h = int(patch_index_h[1:])
-            patch_index_w = int(patch_index_w[1:])
-            try:
-                df = pd.read_json(file_path)
-            except pd.errors.EmptyDataError:
-                print('Note: %s was empty. Skipping.' % file_path)
-                continue
 
-            #pdb.set_trace()
-            for index, line_data in df.iterrows():
-                df['polygon_x'][index] = np.array(df['polygon_x'][index]) + shift_size * patch_index_w
-                df['polygon_y'][index] = np.array(df['polygon_y'][index]) + shift_size * patch_index_h
-            map_data.append(df)
+        for index, line_data in df.iterrows():
+            df['polygon_x'][index] = np.array(df['polygon_x'][index]) + shift_size * patch_index_w
+            df['polygon_y'][index] = np.array(df['polygon_y'][index]) + shift_size * patch_index_h
+        map_data.append(df)
 
-        map_df = pd.concat(map_data)
+    map_df = pd.concat(map_data)
 
-        features = []
-        for index, line_data in map_df.iterrows():
-            #pdb.set_trace()
-            polygon_x, polygon_y = list(line_data['polygon_x']), list(line_data['polygon_y'])
-            # y needs to be negative for geojson
-            polygon = Polygon([[[x,-y] for x,y in zip(polygon_x, polygon_y)]+[[polygon_x[0], -polygon_y[0]]]])
-            text = line_data['text']
-            score = line_data['score']
-            features.append(Feature(geometry = polygon, properties={"text": text, "score": score} ))
+    features = []
+    for index, line_data in map_df.iterrows():
+        polygon_x, polygon_y = list(line_data['polygon_x']), list(line_data['polygon_y'])
+        # y needs to be negative for geojson
+        polygon = Polygon([[[x,-y] for x,y in zip(polygon_x, polygon_y)]+[[polygon_x[0], -polygon_y[0]]]])
+        text = line_data['text']
+        score = line_data['score']
+        features.append(Feature(geometry = polygon, properties={"text": text, "score": score} ))
 
-        feature_collection = FeatureCollection(features)
-        with open(os.path.join(output_dir, map_subdir +'.geojson'), 'w') as f:
-            dump(feature_collection, f)
-        
-        logging.info('Done generating geojson (img coord) for %s', map_subdir)
+    feature_collection = FeatureCollection(features)
+    # with open(os.path.join(output_dir, map_subdir +'.geojson'), 'w') as f:
+    #     dump(feature_collection, f)
+    with open(output_geojson, 'w') as f:
+        dump(feature_collection, f)
+    
+    logging.info('Done generating geojson (img coord) for %s', map_subdir)
+
 
 if __name__ == '__main__':
-    #concatenate_and_convert_to_geojson(input_dir)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_dir', type=str, default='data/100_maps_crop_abc/',
+    parser.add_argument('--input_dir', type=str, default='data/100_maps_crop_abc/0063014',
                         help='path to input json path.')
-    parser.add_argument('--output_dir', type=str, default='data/100_maps_geojson_abc/',
+    
+    parser.add_argument('--output_geojson', type=str, default='data/100_maps_geojson_abc/0063014.geojson',
                         help='path to output geojson path')
+
     parser.add_argument('--shift_size', type=int, default = 1000,
                         help='image patch size and shift size.')
    
--- run.py
+++ run.py
@@ -4,143 +4,218 @@ import argparse
 import time
 import logging
 import pandas as pd
+import pdb
+import datetime
 from utils import get_img_path_from_external_id
 
 logging.basicConfig(level=logging.INFO)
 
+def execute_command(command, if_print_command):
+    t1 = time.time()
+
+    if if_print_command:
+        print(command)
+    os.system(command)
+
+    t2 = time.time()
+    time_usage = t2 - t1 
+    return time_usage
+
+
 def run_pipeline(args):
+    # -------------------------  Pass arguments -----------------------------------------
     map_kurator_system_dir = args.map_kurator_system_dir
     text_spotting_model_dir = args.text_spotting_model_dir
     sample_map_path = args.sample_map_csv_path
-    expt_prefix = args.expt_prefix
+    expt_name = args.expt_name
     output_folder = args.output_folder
 
     module_gen_geotiff = args.module_gen_geotiff
+    module_cropping = args.module_cropping
     module_text_spotting = args.module_text_spotting
     module_img_geojson = args.module_img_geojson 
     module_geocoord_geojson = args.module_geocoord_geojson 
     module_entity_linking = args.module_entity_linking
 
-    #input_csv_path = os.path.join(map_kurator_system_dir ,sample_map_path)
+    if_print_command = args.print_command
+
+    sid_to_jpg_dir = '/data2/rumsey_sid_to_jpg/'
+
+    # ------------------------- Read sample map list and prepare output dir ----------------
     input_csv_path = sample_map_path
     sample_map_df = pd.read_csv(sample_map_path, dtype={'external_id':str})
-
-
-    # geotiff_output_dir = os.path.join(map_kurator_system_dir ,'m1_geotiff/data/geotiff')
-    # cropping_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/'+expt_prefix+'_crop/')
-    # spotting_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/'+expt_prefix+'_crop_outabc/')
-    # stitch_output_dir = os.path.join(map_kurator_system_dir, 'm2_detection_recognition', 'data/'+expt_prefix+'_geojson_abc/')
-    # geojson_output_dir = os.path.join(map_kurator_system_dir, 'm4_geocoordinate_converter', 'data/'+expt_prefix+'_geojson_abc_geocoord/')
-
-    geotiff_output_dir = os.path.join(output_folder, expt_prefix + '_geotiff')
-    cropping_output_dir = os.path.join(output_folder, expt_prefix + '_crop/')
-    spotting_output_dir = os.path.join(output_folder, expt_prefix + '_crop_outabc/')
-    stitch_output_dir = os.path.join(output_folder, expt_prefix + '_geojson_abc/')
-    geojson_output_dir = os.path.join(output_folder, expt_prefix + '_geojson_abc_geocoord/')
-
     external_id_to_img_path_dict = get_img_path_from_external_id( sample_map_path = input_csv_path)
     
+    #time_usage_df = pd.DataFrame({'external_id':sample_map_df['external_id')}) # initialization
+    
+    time_usage_dict = dict()
+    for ex_id in sample_map_df['external_id']:
+        time_usage_dict[ex_id] = {} #{'external_id':ex_id}
+    
+    geotiff_output_dir = os.path.join(output_folder, expt_name,  'geotiff')
+    cropping_output_dir = os.path.join(output_folder, expt_name, 'crop/')
+    spotting_output_dir = os.path.join(output_folder, expt_name,  'crop_outabc/')
+    stitch_output_dir = os.path.join(output_folder, expt_name, 'geojson_abc/')
+    geojson_output_dir = os.path.join(output_folder, expt_name, 'geojson_abc_geocoord/')
+
+   
+    # ------------------------- Generate geotiff ------------------------------
     time_start =  time.time()
     if module_gen_geotiff:
-        # run module1 to generate geotiff
         os.chdir(os.path.join(map_kurator_system_dir ,'m1_geotiff'))
         
         if not os.path.isdir(geotiff_output_dir):
             os.makedirs(geotiff_output_dir)
 
         run_geotiff_command = 'python convert_image_to_geotiff.py --sample_map_path '+ input_csv_path +' --out_geotiff_dir '+geotiff_output_dir  # can change params in argparse
-        print(run_geotiff_command)
-        os.system(run_geotiff_command)
+        execute_command(run_geotiff_command, if_print_command)
 
     time_geotiff = time.time()
-    logging.info('Time for generating geotiff: %d', time_geotiff - time_start)
-
     
+
+    # ------------------------- Image cropping  ------------------------------
+    if module_cropping:
+        for index, record in sample_map_df.iterrows():
+            external_id = record.external_id
+            img_path = external_id_to_img_path_dict[external_id]
+            map_name = os.path.basename(img_path).split('.')[0]
+            
+            if img_path[-4:] == '.sid':
+                # convert sid to jpg
+                redirected_path = os.path.join(sid_to_jpg_dir, map_name + '.jpg')
+
+                mrsiddecode_executable="/home/zekun/dr_maps/mapkurator-system/m1_geotiff/MrSID_DSDK-9.5.4.4709-rhel6.x86-64.gcc531/Raster_DSDK/bin/mrsiddecode"
+
+                run_sid_to_jpg_command = mrsiddecode_executable + ' -quiet -i '+ img_path + ' -o '+redirected_path
+                time_usage = execute_command(run_sid_to_jpg_command, if_print_command)
+                time_usage_dict[external_id]['conversion'] = time_usage
+
+                img_path = redirected_path
+                
+
+            os.chdir(os.path.join(map_kurator_system_dir ,'m2_detection_recognition'))
+            if not os.path.isdir(cropping_output_dir):
+                os.makedirs(cropping_output_dir)
+            run_crop_command = 'python crop_img.py --img_path '+img_path + ' --output_dir '+ cropping_output_dir
+
+            time_usage = execute_command(run_crop_command, if_print_command)
+            time_usage_dict[external_id]['cropping'] = time_usage
+
+    time_cropping = time.time()
+    
+    # ------------------------- Text Spotting (patch level) ------------------------------
     if module_text_spotting:
-        
-        #geotiff_path_list = glob.glob(os.path.join(geotiff_output_dir, '*.geotiff'))
-        #assert(len(geotiff_path_list) != 0)
+        os.chdir(text_spotting_model_dir)
 
         for index, record in sample_map_df.iterrows():
             external_id = record.external_id
             img_path = external_id_to_img_path_dict[external_id]
             map_name = os.path.basename(img_path).split('.')[0]
 
-            if img_path[-4:] == '.sid':
-                continue
-        
-            # for geotiff_path in geotiff_path_list:
-            #     #run module2: image cropping
-            #     os.chdir(os.path.join(map_kurator_system_dir ,'m2_detection_recognition'))
-            #     map_name = os.path.basename(geotiff_path).split('.')[0]
-        
-            os.chdir(os.path.join(map_kurator_system_dir ,'m2_detection_recognition'))
-            if not os.path.isdir(cropping_output_dir):
-                os.makedirs(cropping_output_dir)
-            run_crop_command = 'python crop_img.py --img_path '+img_path + ' --output_dir '+ cropping_output_dir
+            # if img_path[-4:] == '.sid':
+            #     continue
 
-            print(run_crop_command)
-            os.system(run_crop_command)
-        
-            # run module2: text spotting
-            os.chdir(text_spotting_model_dir)
-            # map_name = os.path.basename(geotiff_path).split('.')[0]
-        
             map_spotting_output_dir = os.path.join(spotting_output_dir,map_name)
             if not os.path.isdir(map_spotting_output_dir):
                 os.makedirs(map_spotting_output_dir)
         
             run_spotting_command = 'python demo/demo.py 	--config-file configs/BAText/CTW1500/attn_R_50.yaml 	--input='+ os.path.join(cropping_output_dir,map_name) + '  --output='+ map_spotting_output_dir + '   --opts MODEL.WEIGHTS ctw1500_attn_R_50.pth'
             run_spotting_command  += ' 1> /dev/null'
-            print(run_spotting_command)
-            os.system(run_spotting_command)
+            
+            time_usage = execute_command(run_spotting_command, if_print_command)
+            time_usage_dict[external_id]['spotting'] = time_usage
+
             logging.info('Done text spotting for %s', map_name)
 
     time_text_spotting = time.time()
-    logging.info('Time for text spotting : %d',time_text_spotting - time_geotiff)
+    
 
+    # ------------------------- Image coord geojson (map level) ------------------------------
     if module_img_geojson:
-        # run module2: geojson stitching
         os.chdir(os.path.join(map_kurator_system_dir ,'m2_detection_recognition'))
-        stitch_input_dir = spotting_output_dir
-        
         if not os.path.isdir(stitch_output_dir):
             os.makedirs(stitch_output_dir)
-        run_stitch_command = 'python stitch_output.py --input_dir '+stitch_input_dir + ' --output_dir ' + stitch_output_dir
-        print(run_stitch_command)
-        os.system(run_stitch_command)
+
+        for index, record in sample_map_df.iterrows():
+            external_id = record.external_id
+            img_path = external_id_to_img_path_dict[external_id]
+            map_name = os.path.basename(img_path).split('.')[0]
+
+            # if img_path[-4:] == '.sid':
+            #     continue
+
+            stitch_input_dir = os.path.join(spotting_output_dir, map_name)
+            output_geojson = os.path(stitch_output_dir, map_name + '.geojson')
+            
+            run_stitch_command = 'python stitch_output.py --input_dir '+stitch_input_dir + ' --output_geojson ' + output_geojson
+            time_usage = execute_command(run_stitch_command, if_print_command)
+            time_usage_dict[external_id]['imgcoord_geojson'] = time_usage
 
     time_img_geojson = time.time()
-    logging.info('Time for generating geojson in img coordinate : %d',time_img_geojson - time_text_spotting)
 
+    
+
+    # ------------------------- Convert image coordinates to geocoordinates ------------------------------
     if module_geocoord_geojson:
-        # run module4: convert image coordinates to geocoordinates
         os.chdir(os.path.join(map_kurator_system_dir, 'm4_geocoordinate_converter'))
         
         if not os.path.isdir(geojson_output_dir):
             os.makedirs(geojson_output_dir)
         
         run_converter_command = 'python convert_geojson_to_geocoord.py --sample_map_path '+ input_csv_path +' --in_geojson_dir '+stitch_output_dir +' --out_geojson_dir '+geojson_output_dir
-        print(run_converter_command)
-        os.system(run_converter_command)
+        execute_command(run_converter_command, if_print_command)
 
     time_geocoord_geojson = time.time()
-    logging.info('Time for generating geojson in geo coordinate : %d',time_geocoord_geojson - time_img_geojson)
 
+    
+
+    # ------------------------- Link entities in OSM ------------------------------
+    # To jina: 
+    # remember to change output dir (according to Line41)
+    # time usage logging for each map - write to time_usage_df 
+    #  -zekun
     if module_entity_linking:
-        # run module5: link entities in OSM
         os.chdir(os.path.join(map_kurator_system_dir, 'm5_entity_linker'))
+        
         geojson_linked_output_dir = os.path.join(map_kurator_system_dir, 'm5_entity_linker', 'data/100_maps_geojson_abc_linked/')
         if not os.path.isdir(geojson_output_dir):
             os.makedirs(geojson_output_dir)
 
         run_linker_command = 'python entity_linker.py --sample_map_path '+ input_csv_path +' --in_geojson_dir '+ geojson_output_dir +' --out_geojson_dir '+ geojson_linked_output_dir
-        print(run_linker_command)
-        os.system(run_linker_command)
+        execute_command(run_linker_command, if_print_command)
 
     time_entity_linking = time.time()
+    
+    # --------------------- Time usage logging --------------------------
+    print('\n')
+    logging.info('Time for generating geotiff: %d', time_geotiff - time_start)
+    logging.info('Time for Cropping : %d',time_cropping - time_geotiff)
+    logging.info('Time for text spotting : %d',time_text_spotting - time_cropping)
+    logging.info('Time for generating geojson in img coordinate : %d',time_img_geojson - time_text_spotting)
+    logging.info('Time for generating geojson in geo coordinate : %d',time_geocoord_geojson - time_img_geojson)
     logging.info('Time for entity linking : %d',time_entity_linking - time_geocoord_geojson)
 
+    time_usage_df = pd.DataFrame.from_dict(time_usage_dict, orient='index')
+    time_usage_log_path = os.path.join(output_folder, expt_name, 'time_usage.csv')
+
+    
+    # check if exist time_usage log file 
+    if os.path.isfile(time_usage_log_path):
+        existing_df = pd.read_csv(time_usage_log_path, index_col='external_id', dtype={'external_id':str})
+        # if exist duplicate columns, ret time usage values to the latest run
+        cols_to_use = existing_df.columns.difference(time_usage_df.columns)
+
+        time_usage_df = time_usage_df.join(existing_df[cols_to_use])
+
+        # make sure time_usage_expt_name.csv always have the latest time usage
+        m_time = os.path.getmtime(time_usage_log_path)
+        dt_m = datetime.datetime.fromtimestamp(m_time)
+        timestr = dt_m.strftime("%Y%m%d-%H%M%S") 
+        deprecated_path = os.path.join(output_folder, expt_name, 'time_usage_' +  timestr +'.csv')
+        run_command = 'mv ' + time_usage_log_path + ' ' + deprecated_path
+        execute_command(run_command, if_print_command)
+
+    time_usage_df.to_csv(time_usage_log_path, index_label='external_id')
 
 def main():
     parser = argparse.ArgumentParser()
@@ -148,14 +223,17 @@ def main():
     parser.add_argument('--map_kurator_system_dir', type=str, default='/home/zekun/dr_maps/mapkurator-system/')
     parser.add_argument('--text_spotting_model_dir', type=str, default='/home/zekun/antique_names/model/AdelaiDet/')
     parser.add_argument('--sample_map_csv_path', type=str, default='m1_geotiff/data/sample_US_jp2_100_maps.csv')
-    parser.add_argument('--output_folder', type=str, default='/data2/dr_output')
-    parser.add_argument('--expt_prefix', type=str, default='1000_maps')
+    parser.add_argument('--output_folder', type=str, default='/data2/rumsey_output')
+    parser.add_argument('--expt_name', type=str, default='1000_maps') # output prefix 
     
     parser.add_argument('--module_gen_geotiff', default=False, action='store_true')
+    parser.add_argument('--module_cropping', default=False, action='store_true')
     parser.add_argument('--module_text_spotting', default=False, action='store_true')
     parser.add_argument('--module_img_geojson', default=False, action='store_true')
     parser.add_argument('--module_geocoord_geojson', default=False, action='store_true')
     parser.add_argument('--module_entity_linking', default=False, action='store_true')
+
+    parser.add_argument('--print_command', default=False, action='store_true')
 
                         
     args = parser.parse_args()
