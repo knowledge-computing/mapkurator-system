9be73ec741d10889cff975b4bbd3d6cf03a5402d
Jina Kim <jinakimcs@gmail.com>
2023-08-02 14:59:42 -0500

    Update entity_linking.py to address 'idle' status issue

--- m6_entity_linker/entity_linking.py
+++ m6_entity_linker/entity_linking.py
@@ -50,120 +50,121 @@ def main(args):
     
     try:
         conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD, port=DB_PORT, connection_factory=LinkerLoggingConnection)
-    except:
-        logging.warning('Error on psycopg2 connection')
+    except Exception as e:
+        logging.warning('Error on psycopg2 connection ', e)
         return
-
-    conn.initialize(logger)
-    cur = conn.cursor()
 
     sample_map_df = pd.read_csv(args.sample_map_path, dtype={'image_no': str})
     sample_map_df['image_no'] = sample_map_df['image_no'].str.replace('.1.jp2', '', regex=False).str.replace('.jp2', '', regex=False)
     sample_map_df['image_no'] = sample_map_df['image_no'].apply(lambda x: x[:-2] if x[-2:] == '.1' else x)
 
-    for index, record in sample_map_df.iterrows():
-        input_geojson_file = os.path.join(input_dir, record.image_no + ".geojson")
-	
-        if not os.path.exists(input_geojson_file):
-            logging.warning('PostOCR output does not exist %s', record.image_no + ".geojson")
-            continue
+    conn.initialize(logger)
+    conn.autocommit = True
+
+    with conn.cursor() as cur:
+        for index, record in sample_map_df.iterrows():
+            input_geojson_file = os.path.join(input_dir, record.image_no + ".geojson")
         
-        if os.path.exists(os.path.join(output_dir, input_geojson_file.split("/")[-1])):
-            logging.info('EntityLinker output already exists %s', record.image_no + ".geojson")
-            continue
+            if not os.path.exists(input_geojson_file):
+                logging.warning('PostOCR output does not exist %s', record.image_no + ".geojson")
+                continue
+            
+            if os.path.exists(os.path.join(output_dir, input_geojson_file.split("/")[-1])):
+                logging.info('EntityLinker output already exists %s', record.image_no + ".geojson")
+                continue
 
-        with open(input_geojson_file) as f:
-            try: 
-                data = geojson.load(f)
-            except json.decoder.JSONDecodeError:
-                if os.path.getsize(input_geojson_file) == 0:
-                    with open(os.path.join(output_dir, input_geojson_file.split("/")[-1]), 'w') as fp:
+            with open(input_geojson_file) as f:
+                try: 
+                    data = geojson.load(f)
+                except json.decoder.JSONDecodeError:
+                    if os.path.getsize(input_geojson_file) == 0:
+                        with open(os.path.join(output_dir, input_geojson_file.split("/")[-1]), 'w') as fp:
+                            continue
+                    else:
+                        logging.info('JSONDecodeError %s', input_geojson_file)
                         continue
-                else:
-                    logging.info('JSONDecodeError %s', input_geojson_file)
-                    continue
 
-            for feature_data in data['features']:
-                map_text = str(feature_data['properties']['postocr_label'])
+                for feature_data in data['features']:
+                    map_text = str(feature_data['properties']['postocr_label'])
 
-                # skip null geometry
-                if feature_data['geometry'] is None:
-                    feature_data["properties"]["osm_id"] = []
-                    continue
-                
-                # skip text less than 3 characters
-                if len(map_text) <= 3:
-                    feature_data["properties"]["osm_id"] = []
-                    continue
-
-                pts = np.array(feature_data['geometry']['coordinates']).reshape(-1, 2)
-                map_polygon = Polygon(pts)
-
-                es_query = {
-                    "bool": {
-                    "must": [
-                        {
-                        "match": {'name': map_text.lower().replace("'","\'")}
-                        }
-                    ]
-                    }
-                }
-
-                try:
-                    es_results = es.search(index="osm-linker", query=es_query)
-                except elasticsearch.ElasticsearchException as es_error:
-                    logging.warning('ElasticsearchException while running %s', input_geojson_file.split("/")[-1])
-                    continue
-
-                if es_results['hits']['total']['value'] == 0:
-                    # logging.info('No elasticsearch results of word %s while running %s', map_text, input_geojson_file.split("/")[-1])
-                    feature_data["properties"]["osm_id"] = []
-                    continue
-
-                es_results = [ast.literal_eval(hit["_source"]['source_table_osm_id']) for hit in es_results['hits']['hits']][0]
-                output_osm_ids = []
-                source_tables = set([table for table, _ in es_results])
-
-                for source_table in source_tables:
-                    sql = ""
-                    osm_ids = [osm_id for table, osm_id in es_results if table == source_table]
-
-                    if "points" in source_table:
-                        sql = f"""SELECT osm_id
-                                FROM  {source_table}
-                                WHERE ST_CONTAINS(ST_TRANSFORM(ST_SetSRID(ST_MakeValid('{map_polygon}'), 3857), 4326), wkb_geometry)
-                                AND osm_id = ANY (%s)
-                        """
+                    # skip null geometry
+                    if feature_data['geometry'] is None:
+                        feature_data["properties"]["osm_id"] = []
+                        continue
                     
-                    elif "line" in source_table:
-                        sql = f"""SELECT osm_id
-                                FROM  {source_table}
-                                WHERE ST_INTERSECTS(ST_TRANSFORM(ST_SetSRID(ST_MakeValid('{map_polygon}'), 3857), 4326), wkb_geometry)
-                                AND osm_id = ANY (%s)
-                        """
+                    # skip text less than 3 characters
+                    if len(map_text) <= 3:
+                        feature_data["properties"]["osm_id"] = []
+                        continue
 
-                    elif "polygon" in source_table or "other_relations" in source_table:
-                        sql = f"""SELECT osm_id
-                                FROM  {source_table}
-                                WHERE ST_INTERSECTS(ST_TRANSFORM(ST_SetSRID(ST_MakeValid('{map_polygon}'), 3857), 4326), ST_MakeValid(wkb_geometry, 'method=structure'))
-                                AND osm_id = ANY (%s)
-                        """
+                    pts = np.array(feature_data['geometry']['coordinates']).reshape(-1, 2)
+                    map_polygon = Polygon(pts)
+
+                    es_query = {
+                        "bool": {
+                        "must": [
+                            {
+                            "match": {'name': map_text.lower().replace("'","\'")}
+                            }
+                        ]
+                        }
+                    }
 
                     try:
-                        cur.execute(sql,(osm_ids,))
-                    except Exception as e:
-                        # logging.info('Error occured while executing sql for %s', input_geojson_file.split("/")[-1])
+                        es_results = es.search(index="osm-linker", query=es_query)
+                    except elasticsearch.ElasticsearchException as es_error:
+                        logging.warning('ElasticsearchException while running %s', input_geojson_file.split("/")[-1])
                         continue
+
+                    if es_results['hits']['total']['value'] == 0:
+                        # logging.info('No elasticsearch results of word %s while running %s', map_text, input_geojson_file.split("/")[-1])
+                        feature_data["properties"]["osm_id"] = []
+                        continue
+
+                    es_results = [ast.literal_eval(hit["_source"]['source_table_osm_id']) for hit in es_results['hits']['hits']][0]
+                    output_osm_ids = []
+                    source_tables = set([table for table, _ in es_results if "other_relations" not in table])
+
+                    for source_table in source_tables:
+                        sql = ""
+                        osm_ids = [osm_id for table, osm_id in es_results if table == source_table]
+
+                        if "points" in source_table:
+                            sql = f"""SELECT osm_id
+                                    FROM  {source_table}
+                                    WHERE ST_CONTAINS(ST_TRANSFORM(ST_SetSRID(ST_MakeValid('{map_polygon}'), 3857), 4326), wkb_geometry)
+                                    AND osm_id = ANY (%s)
+                            """
                         
-                    sql_result = cur.fetchall()
-                    if len(sql_result) != 0:
-                        output_osm_ids.extend([x[0] for x in sql_result])
+                        elif "line" in source_table:
+                            sql = f"""SELECT osm_id
+                                    FROM  {source_table}
+                                    WHERE ST_INTERSECTS(ST_TRANSFORM(ST_SetSRID(ST_MakeValid('{map_polygon}'), 3857), 4326), wkb_geometry)
+                                    AND osm_id = ANY (%s)
+                            """
 
-                feature_data["properties"]["osm_id"] = output_osm_ids
+                        elif "polygon" in source_table:
+                            sql = f"""SELECT osm_id
+                                    FROM  {source_table}
+                                    WHERE ST_INTERSECTS(ST_TRANSFORM(ST_SetSRID(ST_MakeValid('{map_polygon}'), 3857), 4326), ST_MakeValid(wkb_geometry, 'method=structure'))
+                                    AND osm_id = ANY (%s)
+                            """
 
-        with open(os.path.join(output_dir, input_geojson_file.split("/")[-1]), 'w', encoding='utf8') as output_geojson:
-            geojson.dump(data, output_geojson, ensure_ascii=False)
-        logging.info('Done generating geojson for %s', input_geojson_file.split("/")[-1])
+                        try:
+                            cur.execute(sql,(osm_ids,))
+                        except Exception as e:
+                            logging.warning('Error occured while executing sql for %s', input_geojson_file.split("/")[-1], e)
+                            return
+                            
+                        sql_result = cur.fetchall()
+                        if len(sql_result) != 0:
+                            output_osm_ids.extend([x[0] for x in sql_result])
+
+                    feature_data["properties"]["osm_id"] = output_osm_ids
+
+            with open(os.path.join(output_dir, input_geojson_file.split("/")[-1]), 'w', encoding='utf8') as output_geojson:
+                geojson.dump(data, output_geojson, ensure_ascii=False)
+            logging.info('Done generating geojson for %s', input_geojson_file.split("/")[-1])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
